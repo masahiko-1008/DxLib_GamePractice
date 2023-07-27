daeb9b9e4e8b03ea000f7532566d2abea39c3b2c
masahiko-1008 <138412620+masahiko-1008@users.noreply.github.com>
2023-07-27 12:19:14 +0900

    制作7

--- GamePrograming/Match3/Stage.cpp
+++ GamePrograming/Match3/Stage.cpp
@@ -29,9 +29,177 @@ typedef struct
 	int x;
 	int y;
 }T_CURSOR;
+
+
 enum 
 {
 	E_NONE,
 	E_ONCE,
 	E_SECOND
 };
+
+T_Object Block[HEIGHT][WIDTH];           //ブロックオブジェクトデータ
+T_CURSOR Select[3];                      //セレクトカーソル座標
+int Item[ITEM_MAX];
+int ClickStatus;
+int Stage_State;
+int Stage_Misson;
+int Stage_Score;
+int ClearFlag;
+
+
+int BlockImage[BLOCK_IMAGE_MAX];         //ブロック画像
+int StageImage;                          //背景用画像
+int ClickSE;                             //クリックSE
+int FadeOutSE;                           //フェードアウトSE
+int MoveBlockSE;                         //ブロック移動SE
+
+
+int combo_check(int x, int y);
+void combo_check_h(int y, int x, int* cnt, int* col);
+void combo_check_w(int y, int x, int* cnt, int* col);
+void save_block(void);
+void restore_block(void);
+
+
+int StageInitialize(void)
+{
+
+	int ret = 0;
+	int i;
+
+	//画像読み込み
+	LoadDivGraph("images/block.png", BLOCK_IMAGE_MAX, BLOCK_IMAGE_MAX, 1,
+BLOCKSIZE, BLOCKSIZE, BlockImage);
+	StageImage = LoadGraph("images/stage.png");
+
+	//音源読み込み
+	ClickSE = LoadSoundMem("sounds/click_se.mp3");
+	FadeOutSE = LoadSoundMem("sounds/fadeout_se.mp3");
+	MoveBlockSE = LoadSoundMem("sounds/movebllock_se.mp3");
+
+
+	//ブロック生成処理
+	CreateBlock();
+
+	ClickStatus = E_NONE;
+	Stage_State = 0;
+	Stage_Score = 0;
+	ClearFlag = FALSE;
+
+	for (i = 0; i < 3; i++)
+	{
+		Select[i].x = 0;
+		Select[i].y = 0;
+	}
+
+	//エラーチェック
+	for (i = 0; i < BLOCK_IMAGE_MAX; i++)
+	{
+		if (BlockImage[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+	if (StageImage == -1)
+	{
+		ret = -1;
+	}
+	if (ClickSE == -1)
+	{
+		ret = -1;
+	}
+	if (FadeOutSE == -1)
+	{
+		ret = -1;
+	}
+	if (MoveBlockSE == -1)
+	{
+		ret = -1;
+	}
+
+	return ret;
+}
+
+void StageDraw(void) {
+	DrawGraph(0, 0, StageImage, FALSE);
+
+	//アイテムの取得個数を描画
+	for(int i=0;i<ITEM_MAX;i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, 0xffffff, "%3d", Item[i]);
+	}
+
+	//ブロックを描画
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (Block[i][j].flg == TRUE && Block[i][j].image != NULL)
+			{
+				DrawGraph(Block[i][j].x, Block[i][j].y,
+					BlockImage[Block[i][j].image], TRUE);
+			}
+		}
+	}
+
+	//選択ブロックを描画
+	DrawGraph(Select[SELECT_CURSOR].x * BLOCKSIZE, Select[SELECT_CURSOR].y *
+		BLOCKSIZE, BlockImage[9], TRUE);
+	if (ClickStatus != E_NONE)
+	{
+	DrawGraph(Select[NEXT_CURSOR].x * BLOCKSIZE,
+		Select[NEXT_CURSOR].y *BLOCKSIZE, BlockImage[9], TRUE);
+	}
+
+	//ミッションを描画
+	SetFontSize(20);
+	DrawFormatString(590, 211, GetColor(255, 255, 255), "%3d", Stage_Mission);
+
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, GetColor(255, 255, 255), "%3d",
+			Item[i]);
+	}
+}
+
+void CreateBlock(void)
+{
+	int Check = 0;
+	int i, j;
+
+	do
+
+	{
+		Check = 0;
+		for (i = 0; j < HEIGHT; j++)
+		{
+			for (j = 0; j < WIDTH; j++)
+			{
+				if (j == 0 || j == WIDTH - 1 || i == HEIGHT - 1 || i == 0)
+				{
+					Block[i][j].flg = FALSE;
+					Block[i][j].image = NULL;
+				}
+				else
+				{
+					Block[i][j].flg = TRUE;
+					Block[i][j].x(j - 1)* BLOCKSIZE;
+					Block[i][j].y(i - 1)* BLOCKSIZE;
+					Block[i][j].width = BLOCKSIZE;
+					Block[i][j].height = BLOCKSIZE;
+					Block[i][j].image = GetRand(7) + 1;//1～8の乱数
+				}
+			}
+		}
+
+		/*for(i=1;i<HEIGHT-1;i++)
+		{
+		for(j=1;j<WIDTH-1;j++)
+
+	}
+}
