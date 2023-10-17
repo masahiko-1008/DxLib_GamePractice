7fa2e4a7c94ee4c6b91934bb2ff85978d7358d16
masahiko-1008 <138412620+masahiko-1008@users.noreply.github.com>
2023-10-17 13:19:27 +0900

    制作5

--- GamePrograming/Tetoris/Block.cpp
+++ GamePrograming/Tetoris/Block.cpp
@@ -201,6 +201,142 @@ void Block_Update(void)
 		{
 			//ブロックの固定
 			lock_block(DropBlock_X, DropBlock_Y);
+			//ブロックの消去とブロックを下ろす処理
+			check_line();
+			//新しいブロックの生成
+			create_block();
+		}
+		//カウンタの初期化
+		WaitTime = 0;
+	}
+}
+
+//ブロック機能：描画処理
+//引数：なし
+//戻り値：なし
+
+void Block_Draw(void) 
+{
+	int i, j;  //ループカウンタ
+
+	//フィールドのブロックを描画
+	for (i = 0; i < FIELD_HEIGHT; i++)
+	{
+		for (j = 0; j < FIELD_WIDTH; j++)
+		{
+			if (Field[i][j] != E_BLOCK_WALL)
+			{
+				DrawGraph(j * BLOCK_SIZE, i * BLOCK_SIZE, BlockImage[Field[i][j]],
+					TRUE);
+			}
 		}
 	}
+	//次のブロックとストックされたブロックを描画
+	for (i = 0; i < BLOCK_TROUT_SIZE; i++)
+	{
+		for (j = 0; j < BLOCK_TROUT_SIZE; j++)
+		{
+			//次のブロックを描画
+			DrawGraph(BLOCK_SIZE * j + BLOCK_NEXT_POS_X, BLOCK_SIZE * i +
+BLOCK_NEXT_POS_Y, BlockImage[Next[i][j]], TRUE);
+			//ストックされたブロックえお描画
+			DrawGraph(BLOCK_SIZE * j + BLOCK_NEXT_POS_X, BLOCK_SIZE * i +
+BLOCK_STOCK_POS_Y, BlockImage[Stock[i][j]], TRUE);
+		}
+	}
+	//落ちてくるブロックの描画
+	for (i = 0; i < BLOCK_TROUT_SIZE; i++)
+	{
+		for (j = 0; j < BLOCK_TROUT_SIZE; j++)
+		{
+			DrawGraph((DropBlock_X + j) * BLOCK_SIZE, (DropBlock_Y + 1) * BLOCK_SIZE,
+BlockImage[DropBlock[i][j]], TRUE);
+		}
+	}
+}
+
+//ブロック機能：ブロックの生成判定処理
+//引数：なし
+//戻り値：TRUE(ブロックの生成ができる),FALSE(生成不可)
+
+int Get_GenerateFlg(void)
+{
+	return Generate_Flg;
+}
+
+//ブロック機能：消したラインの数取得処理
+//引数：なし
+//戻り値：消したラインの数
+
+int Get_Line(void)
+{
+	return DeleteLine;
+}
+
+//ブロック機能：フィールド生成処理
+//引数：なし
+//戻り値：なし
+
+void create_field(void)
+{
+	int i, j;  //ループカウンタ
+
+	//フィールドの生成
+	for (i = 0; i < FIELD_HEIGHT; i++)
+	{
+		for (j = 0; j < FIELD_WIDTH; j++)
+		{
+			//フィールド値の設定
+			if (j == 0 || j == FIELD_WIDTH - 1 || i == FIELD_HEIGHT - 1)
+			{
+				Field[i][j] = E_BLOCK_WALL;  //壁状態にする
+			}
+			else
+			{
+				Field[i][j] = E_BLOCK_EMPTY;  //空状態にする
+			}
+		}
+	}
+}
+
+//ブロック機能：ブロック生成処理
+//引数：なし
+//戻り値：なし
+
+void create_block(void)
+{
+	int i, j; //ループカウンタ
+	int block_type;  //次に出現させるブロックタイプ
+
+	//次に出現させるブロックの決定する
+	block_type = GetRand(BLOCK_TYPE_MAX - 1);
+
+	//新しいブロックをセット＆次のブロックを生成
+	for (i = 0; i < BLOCK_TROUT_SIZE; j++)
+	{
+		for (j = 0; j < BLOCK_TROUT_SIZE; j++)
+		{
+			DropBlock[i][j] = Next[i][j];
+			Next[i][j] = (BLOCK_STATE)C_BLOCK_TABLE[block_type][i][j];
+		}
+	}
+	
+	//出現位置の設定
+	DropBlock_X = DROP_BLOCK_INIT_X;
+	DropBlock_Y = DROP_BLOCK_INIT_Y;
+
+	//生成できなかった時、ゲームオーバーに遷移する
+	if (check_overlap(DropBlock_X, DropBlock_Y) == FALSE)
+	{
+		Generate_Flg = FALSE;
+	}
+}
+
+//ブロック機能：ブロックの移動処理
+//引数：なし
+//戻り値：なし
+
+void move_block(void)
+{
+
 }
